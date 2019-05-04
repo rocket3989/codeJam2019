cda8ea6c031e1cf0fe892aa71ce2da9bca6cec09
rocket3989 <rocket3989@gmail.com>
2019-05-04 08:03:10 -0500

    removed copies

--- Round1C/Bacteria.py
+++ Round1C/Bacteria.py
@@ -7,8 +7,8 @@ def check(state, turn, R, C):
         for c in range(C):
             if state[r][c] == '.':
                 
-                newstate = copy.deepcopy(state)
-                newstate[r][c] = '0'
+                changed = [r]
+                state[r][c] = '0'
                 rI = r + 1
                 madeIt = True
                 while rI < R:
@@ -17,7 +17,8 @@ def check(state, turn, R, C):
                     if state[rI][c] == '#':
                         madeIt = False
                         break
-                    newstate[rI][c] = '0'
+                    state[rI][c] = '0'
+                    changed.append(rI)
                     rI += 1
 
                 rI = r - 1
@@ -27,17 +28,23 @@ def check(state, turn, R, C):
                     if state[rI][c] == '#':
                         madeIt = False
                         break
-                    newstate[rI][c] = '0'
+                    state[rI][c] = '0'
+                    changed.append(rI)
                     rI -= 1
                 if madeIt:
-                    output = check(newstate, (turn + 1) % 2, R, C)
+                    output = check(state, (turn + 1) % 2, R, C)
                     if output[turn]:
                         count[turn] += 1
                     else: count[(turn + 1) % 2] += 1
                     found = True
 
-                newstate = copy.deepcopy(state)
-                newstate[r][c] = '0'
+                for i in changed:
+                    state[i][c] = '.'
+
+
+
+                changed = [c]
+                state[r][c] = '0'
                 cI = c + 1
                 madeIt = True
                 while cI < C:
@@ -46,7 +53,8 @@ def check(state, turn, R, C):
                     if state[r][cI] == '#':
                         madeIt = False
                         break
-                    newstate[r][cI] = '0'
+                    state[r][cI] = '0'
+                    changed.append(cI)
                     cI += 1
                     
                 cI = c - 1
@@ -56,15 +64,18 @@ def check(state, turn, R, C):
                     if state[r][cI] == '#':
                         madeIt = False
                         break
-                    newstate[r][cI] = '0'
+                    state[r][cI] = '0'
+                    changed.append(cI)
                     cI -= 1
                 if madeIt:
-                    output = check(newstate, (turn + 1) % 2, R, C)
+                    output = check(state, (turn + 1) % 2, R, C)
                     if output[turn]:
                         count[turn] += 1
                     else: count[(turn + 1) % 2] += 1
                     found = True
-                    
+                for i in changed:
+                    state[r][i] = '.'
+
     if not found:
         count[(turn + 1) % 2] = 1
     return count
